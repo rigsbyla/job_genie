4bb390ae2a78657d909a83c5dd783cc7416ede89
rigsbyla <rigsbyla@oregonstate.edu>
2026-05-03 07:17:20 -0700

    Enhance DataTable with filtering options and improve job query ordering

--- app/job_board/page.tsx
+++ app/job_board/page.tsx
@@ -1,17 +1,18 @@
-import { columns } from "./columns"
-import { DataTable } from "./data-table"
+import { Suspense } from 'react'
+import { DataTable } from './data-table'
+import { columns } from './columns'
 
 async function getData() {
   const res = await fetch('http://localhost:8000/jobs', { cache: 'no-store' })
   return res.json()
 }
 
-export default async function JobsPage() {
+export default async function JobBoardPage() {
   const data = await getData()
 
   return (
-    <div className="container mx-auto py-10">
+    <Suspense fallback={<div>Loading...</div>}>
       <DataTable columns={columns} data={data} />
-    </div>
+    </Suspense>
   )
 }
